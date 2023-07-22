b57b860ab7c6f045452d11eaaa881a98edfbe359
imagejung <ysjung9312@gmail.com>
2023-07-23 02:00:30 +0900

    Edit / video / 스켈레톤 이미지 최적화

--- Frontend/public/app.js
+++ Frontend/public/app.js
@@ -22,8 +22,8 @@ const addSkeletonButton = document.getElementById("addSkeletonButton");
 addSkeletonButton.addEventListener("click", addSkeletonToVideo);
 const canvas = document.getElementById("canvas");
 const canvasCtx5 = canvas.getContext('2d');
-
 const video = document.getElementById('fileDisplay');
+let skeletonEnabled = false;
 
 
 call.hidden = true;
@@ -405,50 +405,49 @@ videoPlayer.addEventListener("seeked", (event) => {
 });
 
 
-
-
-
-// 동영상 위에 스켈레톤 이미지를 씌우는 함수
 const pose = new Pose({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.2/${file}`;
   }});
-console.log("Pose object created: ", pose);
 
+
+// 동영상 위에 스켈레톤 이미지를 씌우는 함수
 async function addSkeletonToVideo() {
-  console.log("addSkeletonToVideo called")
-  // 이미 스켈레톤이 적용되어있으면 함수 종료
+  skeletonEnabled = !skeletonEnabled; // 버튼 클릭시 토글
+  if(skeletonEnabled) { // 스켈레톤 활성화시 pose 설정
+    pose.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    });
 
-  pose.setOptions({
-    modelComplexity: 1,
-    smoothLandmarks: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-  });
+    pose.onResults(onResultsPose);
 
-  pose.onResults(onResultsPose);
+    // 비디오 플레이어가 로드되면 포즈를 감지합니다.
+    videoPlayer.oncanplay = () => {
+      console.log("onloadstarted");
+      window.skeletonApplied = true;
 
-  // 비디오 플레이어가 로드되면 포즈를 감지합니다.
-  console.log("Before setting videoPlayer.onloadedmetadata");
-  console.log("Video player src2: ", videoPlayer.src);
-  videoPlayer.oncanplay = () => {
-    console.log("onloadstarted");
-    window.skeletonApplied = true;
+      // Set the canvas size to the video size
+      canvas.width = videoPlayer.videoWidth;
+      canvas.height = videoPlayer.videoHeight;
 
-    // Set the canvas size to the video size
-    canvas.width = videoPlayer.videoWidth;
-    canvas.height = videoPlayer.videoHeight;
+      const updatePose = async () => {
+        const currentTime = videoPlayer.currentTime;
+        try {
+          // pass the video element instead of the time
+          await pose.send({ image: videoPlayer });
+        } catch (error) {
+          console.error("Error in pose.send:", error);
+        }
+        // call the next frame
+        requestAnimationFrame(updatePose);
+      };
 
-    // 현재 동영상의 시간이 변경될 때마다 스켈레톤을 그립니다.
-    videoPlayer.ontimeupdate = () => {
-      const currentTime = videoPlayer.currentTime;
-      // 현재 동영상의 시간을 기준으로 스켈레톤 이미지를 그립니다.
-      console.log("Before pose.send call");
-      console.log(videoPlayer);
-      console.log(currentTime);
-      pose.send({ image: videoPlayer, timestamp: currentTime });
-      console.log("After pose.send call");
+      // start the loop
+      updatePose();
     };
-  };
+  }
 }
 
 function zColor(data) {
@@ -456,49 +455,54 @@ function zColor(data) {
   return `rgba(0, ${255 * z}, ${255 * (1 - z)}, 1)`;
 }
 
-
 // Modify the onResults function
 function onResultsPose(results) {
-  console.log("onResultsPose called with results:");
-  console.log(results);
+  if(skeletonEnabled) { // 스켈레톤이 활성화되어 있을 때만 그림
+    console.log("onResultsPose called with results:");
+    console.log(results);
 
-  canvasCtx5.save();
-  canvasCtx5.clearRect(0, 0, canvas.width, canvas.height);
-  canvasCtx5.drawImage(
-      results.image, 0, 0, canvas.width, canvas.height);
-  drawConnectors(
-      canvasCtx5, results.poseLandmarks, POSE_CONNECTIONS, {
-        color: (data) => {
-          const x0 = canvas.width * data.from.x;
-          const y0 = canvas.height * data.from.y;
-          const x1 = canvas.width * data.to.x;
-          const y1 = canvas.height * data.to.y;
+    canvasCtx5.save();
+    canvasCtx5.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx5.drawImage(
+        results.image, 0, 0, canvas.width, canvas.height);
+    drawConnectors(
+        canvasCtx5, results.poseLandmarks, POSE_CONNECTIONS, {
+          color: (data) => {
+            const x0 = canvas.width * data.from.x;
+            const y0 = canvas.height * data.from.y;
+            const x1 = canvas.width * data.to.x;
+            const y1 = canvas.height * data.to.y;
 
-          const z0 = clamp(data.from.z + 0.5, 0, 1);
-          const z1 = clamp(data.to.z + 0.5, 0, 1);
+            const z0 = clamp(data.from.z + 0.5, 0, 1);
+            const z1 = clamp(data.to.z + 0.5, 0, 1);
 
-          const gradient = canvasCtx5.createLinearGradient(x0, y0, x1, y1);
-          gradient.addColorStop(
-              0, `rgba(0, ${255 * z0}, ${255 * (1 - z0)}, 1)`);
-          gradient.addColorStop(
-              1.0, `rgba(0, ${255 * z1}, ${255 * (1 - z1)}, 1)`);
-          return gradient;
-        }
-      });
-  drawLandmarks(
-      canvasCtx5,
-      Object.values(POSE_LANDMARKS_LEFT)
-          .map(index => results.poseLandmarks[index]),
-      {color: zColor, fillColor: '#FF0000'});
-  drawLandmarks(
-      canvasCtx5,
-      Object.values(POSE_LANDMARKS_RIGHT)
-          .map(index => results.poseLandmarks[index]),
-      {color: zColor, fillColor: '#00FF00'});
-  drawLandmarks(
-      canvasCtx5,
-      Object.values(POSE_LANDMARKS_NEUTRAL)
-          .map(index => results.poseLandmarks[index]),
-      {color: zColor, fillColor: '#AAAAAA'});
-  canvasCtx5.restore();
+            const gradient = canvasCtx5.createLinearGradient(x0, y0, x1, y1);
+            gradient.addColorStop(
+                0, `rgba(0, ${255 * z0}, ${255 * (1 - z0)}, 1)`);
+            gradient.addColorStop(
+                1.0, `rgba(0, ${255 * z1}, ${255 * (1 - z1)}, 1)`);
+            return gradient;
+          }
+        });
+    drawLandmarks(
+        canvasCtx5,
+        Object.values(POSE_LANDMARKS_LEFT)
+            .map(index => results.poseLandmarks[index]),
+        {color: zColor, fillColor: '#FF0000'});
+    drawLandmarks(
+        canvasCtx5,
+        Object.values(POSE_LANDMARKS_RIGHT)
+            .map(index => results.poseLandmarks[index]),
+        {color: zColor, fillColor: '#00FF00'});
+    drawLandmarks(
+        canvasCtx5,
+        Object.values(POSE_LANDMARKS_NEUTRAL)
+            .map(index => results.poseLandmarks[index]),
+        {color: zColor, fillColor: '#AAAAAA'});
+    canvasCtx5.restore();
+  } else {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 }
+
