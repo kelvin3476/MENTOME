d73ac1980e83c965e9d084dc52b1be99aaf35f74
kr-younghoon <sullanta0802@gmail.com>
2023-07-27 01:38:00 +0900

    feat🛠️: ui2

--- Frontend/public/src/whiteboard.js
+++ Frontend/public/src/whiteboard.js
@@ -4,10 +4,10 @@ let lastX = 0;
 let lastY = 0;
 
 function drawLine(x1, y1, x2, y2) {
-    const canvas = document.getElementById("drawcanvas");
-    const canvasCtx = canvas.getContext("2d");
+    const canvas = document.getElementById('drawcanvas');
+    const canvasCtx = canvas.getContext('2d');
     canvasCtx.beginPath();
-    canvasCtx.strokeStyle = "black";
+    canvasCtx.strokeStyle = 'black';
     canvasCtx.lineWidth = 2;
     canvasCtx.moveTo(x1, y1);
     canvasCtx.lineTo(x2, y2);
@@ -17,20 +17,33 @@ function drawLine(x1, y1, x2, y2) {
 
 function drawAndEmit(x1, y1, x2, y2, roomName) {
     drawLine(x1, y1, x2, y2);
-    socket.emit("drawing", { x1, y1, x2, y2, roomName });
+    socket.emit('drawing', { x1, y1, x2, y2, roomName });
 }
 
+const drawCanvas = document.getElementById('drawcanvas');
+const toggleDrawCanvasButton = document.getElementById('toggleDrawCanvasButton');
+
+toggleDrawCanvasButton.addEventListener('click', () => {
+    if (drawCanvas.style.display === 'none') {
+        // Show the draw canvas
+        drawCanvas.style.display = 'block';
+    } else {
+        // Hide the draw canvas
+        drawCanvas.style.display = 'none';
+    }
+});
+
 window.onload = () => {
-    const canvas = document.getElementById("drawcanvas");
-    canvas.addEventListener("mousedown", (e) => {
+    const canvas = document.getElementById('drawcanvas');
+    canvas.addEventListener('mousedown', (e) => {
         isDrawing = true;
         [lastX, lastY] = [e.offsetX, e.offsetY];
 
         // Emit a 'start_drawing' event to the server when the drawing starts
-        socket.emit("start_drawing", { x: lastX, y: lastY, roomName });
+        socket.emit('start_drawing', { x: lastX, y: lastY, roomName });
     });
 
-    canvas.addEventListener("mousemove", (e) => {
+    canvas.addEventListener('mousemove', (e) => {
         if (!isDrawing) return;
         const x = e.offsetX;
         const y = e.offsetY;
@@ -38,20 +51,20 @@ window.onload = () => {
         [lastX, lastY] = [x, y];
     });
 
-    canvas.addEventListener("mouseup", () => {
+    canvas.addEventListener('mouseup', () => {
         isDrawing = false;
     });
 
-    canvas.addEventListener("mouseleave", () => {
+    canvas.addEventListener('mouseleave', () => {
         isDrawing = false;
     });
 
     // // In the client-side code, after the Socket.IO connection is established
-    socket.on("drawing", (data) => {
+    socket.on('drawing', (data) => {
         drawLine(data.x1, data.y1, data.x2, data.y2);
     });
 
-    socket.on("start_drawing", (data) => {
+    socket.on('start_drawing', (data) => {
         [lastX, lastY] = [data.x, data.y];
         isDrawing = true;
     });
