1bbdffcefbe683a91600ae58fa38f87372f6b5c6
kr-younghoon <sullanta0802@gmail.com>
2023-07-27 22:52:25 +0900

    Feat: App. Router 코드 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/Home';
+import Posting from './Pages/posting';
+import Mypage from './Pages/myPage';
+import Mentor from './Pages/mentor';
+import Mentee from './Pages/mentee';
+import Actset from './Pages/actset';
+import Post from './Pages/post';
+import RootLayout from './Pages/Root';
+import PostDetail from '../src/components/Post/PostDetail';
+
+import Signup from './Pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* RootLayout을 기준으로 경로에 따라 컴포넌트를 렌더링 */}
+                <Route path="/" element={<RootLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="mentor" element={<Mentor />}>
+                        <Route index element={<Mentor />} />
+                        <Route path=":postId" element={<Post />} />
+                    </Route>
+                    {/* Mentee 관련 라우트 */}
+                    <Route path="mentee" element={<Mentee />}>
+                        <Route index element={<Mentee />} />
+                        <Route path=":postId" element={<Post />} />
+                    </Route>
+                    <Route path="post" element={<Post />} />
+                    <Route path="posting" element={<Posting />} />
+                    <Route path="/PostDetail/:id" element={<PostDetail />} />
+                    <Route path="signup" element={<Signup />} />
+                    <Route path="mypage" element={<Mypage />} />
+                    <Route path="account-setting" element={<Actset />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
