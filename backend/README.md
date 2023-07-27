c4e6e286d04035d7f4eb0b520056af2225d9eeb6
kr-younghoon <sullanta0802@gmail.com>
2023-07-27 22:31:08 +0900

    Add: backend folder

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+Backend server
+
+'npm start' to run server
+
+'npm run dev' to run server to developer
---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        required: true
+    },
+    mentoringCount: {
+        type: Number,
+        required: true
+    },
+    notice: [
+        {
+            noticeType: { type: String, required: true },
+            noticeSender: { type: String, required: true },
+            roomName: { type: String }
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema);
