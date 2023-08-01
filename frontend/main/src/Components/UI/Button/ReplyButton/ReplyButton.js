5e3a1dd8f1cbc2cd9acbf2bc3ea966ddd14e1e6e
kr-younghoon <sullanta0802@gmail.com>
2023-08-02 06:18:57 +0900

    Edit: ReplyButton issue complete

--- frontend/main/src/Components/UI/Button/ReplyButton/ReplyButton.js
+++ frontend/main/src/Components/UI/Button/ReplyButton/ReplyButton.js
@@ -4,7 +4,7 @@ import MinusReplyButton from '../MinusReply/MinusReplyButton';
 import PlusReplyButton from '../PlusReply/PlusReplyButton';
 import styles from './ReplyButton.module.css';
 
-const ReplyButton = ({ id, username, date, replyContent }) => {
+const ReplyButton = ({ id, reply }) => {
     const [showReplyBox, setShowReplyBox] = useState(false); // State variable for showing/hiding reply box
     const [showReplyReplyBox, setShowReplyReplyBox] = useState(false); // State variable for showing/hiding reply box
 
@@ -26,43 +26,42 @@ const ReplyButton = ({ id, username, date, replyContent }) => {
                     <div className={styles.Reply__comment_group}>
                         <div className={styles.Reply__comment_margin_top}></div>
                         <div>
-                            <div className={styles.Reply__comment_padding_top_down}>
-                                <div className={styles.Reply__comment_profile_head}>
-                                    <div className={styles.Reply__comment_profile}>
-                                        {/* a태그 생략 */}
-                                        <img
-                                            src='https://velog.velcdn.com/images/1yoouoo/profile/27630138-254c-45cd-99a6-b9adeadbffc9/image.avif'
-                                            alt='comment-user-thumbnail'
-                                        ></img>
-                                        <div className={styles.Reply__comment_info}>
-                                            <div className={styles.Reply__comment_username}>
-                                                {/* eslint-disable-next-line */}
-                                                <a>{username}</a>
+                            {reply.map((reply) => (
+                                <div className={styles.Reply__comment_padding_top_down}>
+                                    <div className={styles.Reply__comment_profile_head}>
+                                        <div className={styles.Reply__comment_profile}>
+                                            {/* a태그 생략 */}
+                                            <img
+                                                src='https://velog.velcdn.com/images/1yoouoo/profile/27630138-254c-45cd-99a6-b9adeadbffc9/image.avif'
+                                                alt='comment-user-thumbnail'
+                                            ></img>
+                                            <div className={styles.Reply__comment_info}>
+                                                <div className={styles.Reply__comment_username}>
+                                                    {/* eslint-disable-next-line */}
+                                                    <a>{reply.replyWriter}</a>
+                                                </div>
+                                                <div className={styles.Reply__comment_date}>{reply.replyDate}</div>
                                             </div>
-                                            <div className={styles.Reply__comment_date}>{date}</div>
+                                        </div>
+                                        <div className={styles.Reply__comment_actions}>
+                                            <span>삭제</span>
                                         </div>
                                     </div>
-                                    <div className={styles.Reply__comment_actions}>
-                                        <span>삭제</span>
+                                    <div className={styles.Reply__comment_detail_head}>
+                                        <div className={styles.Reply__comment_detail_body}>
+                                            <p>{reply.replyContent}</p>
+                                        </div>
                                     </div>
+                                    <div className={styles.Reply__comment_detail_line}></div>
                                 </div>
-                                <div className={styles.Reply__comment_detail_head}>
-                                    <div className={styles.Reply__comment_detail_body}>
-                                        <p>{replyContent}</p>
-                                    </div>
-                                </div>
-                                <div className={styles.Reply__comment_detail_line}></div>
-                                {!showReplyReplyBox ? (
-                                    <button
-                                        onClick={toggleReplyReplyBox}
-                                        className={styles.Reply__comment_Reply_button}
-                                    >
-                                        답글 작성하기
-                                    </button>
-                                ) : (
-                                    <CommentTextarea id={id} onClick={toggleReplyReplyBox} />
-                                )}
-                            </div>
+                            ))}
+                            {!showReplyReplyBox ? (
+                                <button onClick={toggleReplyReplyBox} className={styles.Reply__comment_Reply_button}>
+                                    답글 작성하기
+                                </button>
+                            ) : (
+                                <CommentTextarea id={id} onClick={toggleReplyReplyBox} />
+                            )}
                         </div>
                     </div>
                 </>
