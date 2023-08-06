23c4fc472de222bc44e69db7a65f5246ebd48086
kijen723 <kij2646@gmail.com>
2023-08-06 11:37:13 +0900

    exitbutton activate

--- frontend/meeting/src/enterroom/enterroom.js
+++ frontend/meeting/src/enterroom/enterroom.js
@@ -1,5 +1,11 @@
+const exitButton = document.getElementById('exit');
+
 function autoEnterRoom() {
     console.log("do it")
     handleWelcomeSubmit();
     handleRoomSubmit();
 }
+
+exitButton.addEventListener('click', () => {
+    window.location.href = '/';
+});
