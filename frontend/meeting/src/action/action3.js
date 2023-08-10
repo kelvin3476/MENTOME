6eb881ae5362825f8accaaf6daf64807dbc47802
kelvin3476 <winrain03226@gmail.com>
2023-08-10 21:59:06 +0900

    Edit/Meeting-UX/toggleUploadbutton dropdown 수정중 및 로딩스피너 merge issue 해결

--- frontend/meeting/src/action/action3.js
+++ frontend/meeting/src/action/action3.js
@@ -52,11 +52,11 @@ ImpactButton.addEventListener('click', () => {
     toggleImpactTooltipVisibility();
 });
 
-// toggleUploadbutton 다른곳 클릭시 드랍다운 메뉴 안보이게 설정
-const toggleUploadbutton = document.getElementById('upload-dropdown-button');
+// // toggleUploadbutton 다른곳 클릭시 드랍다운 메뉴 안보이게 설정
+// const toggleUploadbutton = document.getElementById('upload-dropdown-button');
 
-toggleUploadbutton.addEventListener('blur', () => {
-    const dropdown = document.getElementById('upload-dropdown-content');
-    dropdown.style.display = 'none';
-    dropdown.style.opacity = 0;
-});
+// toggleUploadbutton.addEventListener('blur', () => {
+//     const dropdown = document.getElementById('upload-dropdown-content');
+//     dropdown.style.display = 'none';
+//     dropdown.style.opacity = 0;
+// });
