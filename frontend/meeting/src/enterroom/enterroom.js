1629ff854c2dc76387c0b8a50bd40e2def71d4a9
kr-younghoon <sullanta0802@gmail.com>
2023-08-11 16:36:20 +0900

    Feat: 미팅룸 입장(본인) 효과음 적용

--- frontend/meeting/src/enterroom/enterroom.js
+++ frontend/meeting/src/enterroom/enterroom.js
@@ -1,11 +1,41 @@
+const enterSound = document.getElementById('enterSound');
+const exitSound = document.getElementById('exitSound');
+
+// 상대방이 입장했을 때 효과음 재생
+function playEnterSound() {
+    enterSound.currentTime = 0; // 재생 위치를 처음으로 되돌림
+    enterSound.play();
+}
+
+// 상대방이 퇴장했을 때 효과음 재생
+function playExitSound() {
+    exitSound.currentTime = 0; // 재생 위치를 처음으로 되돌림
+    exitSound.play();
+}
+
+// 상대방 입장 및 퇴장 시 효과음 재생
+// 이 함수를 호출하면 효과음이 재생됩니다.
+function handleUserEnter() {
+    playEnterSound();
+}
+
+function handleUserExit() {
+    playExitSound();
+}
+
 const exitButton = document.getElementById('exit');
 
 function autoEnterRoom() {
-    console.log("do it")
+    console.log('do it');
     handleWelcomeSubmit();
     handleRoomSubmit();
+
+    handleUserEnter(); // 영훈 입장 효과음
+    console.log('dodoit;');
 }
 
 exitButton.addEventListener('click', () => {
+    handleUserExit();
+    console.log('dodododo');
     window.location.href = '/';
 });
